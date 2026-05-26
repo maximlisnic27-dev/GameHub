9c40558acaa32fe55e7ac0aa36f56a911a984aa0
maximlisnic27-dev <maximlisnic27@gmail.com>
2026-05-26 10:22:23 +0300

    Afisare hello world

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test File</title>
+</head>
+<body>
+
+<h1>PHP Demo</h1>
+
+<?php
+    echo "<p>Hello World (afișat în browser)</p>";
+
+    $message = "Hello din PHP!";
+    
+    echo "<p>Mesaj: " . $message . "</p>";
+
+?>
+
+</body>
+</html>
